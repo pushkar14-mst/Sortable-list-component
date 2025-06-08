a59b2b5f6c0f3e9dc7b5c34711ba96d4509537d5
Pushkar <pushkar14.patil@gmail.com>
2025-06-08 00:04:51 -0400

    initial hero section iteration

--- src/widgets/MainContainer/MainContainer.tsx
+++ src/widgets/MainContainer/MainContainer.tsx
@@ -3,7 +3,7 @@ import SkillSelectorContainer from "./ui/SkillSelectorContainer";
 const MainContainer: React.FC = () => {
   return (
     <>
-      <div className="flex h-full max-h-[500px] w-full max-w-[720px] rounded-lg bg-white p-[64px] shadow-lg">
+      <div className="flex h-full max-h-[500px] w-full max-w-[720px] rounded-lg bg-white p-[64px] shadow-[0px_44px_64px_rgba(39,99,235,0.1)]">
         <SkillSelectorContainer />
       </div>
     </>
